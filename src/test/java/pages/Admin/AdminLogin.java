bc30e00e4a30b1014aa4bfc9864d058863088012
sumeyyeuz <140701773+sumeyyeuz@users.noreply.github.com>
2023-12-16 17:26:12 +0300

    16.12.2023 sumeyye admin login method

--- src/test/java/pages/Admin/AdminLogin.java
+++ src/test/java/pages/Admin/AdminLogin.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class AdminLogin {
 
@@ -13,19 +14,24 @@ public class AdminLogin {
     }
 
     @FindBy(xpath = "(//input[@class='form-control'])[1]")
-    public WebElement usernameTextBox;
+    private WebElement usernameTextBox;
     @FindBy (xpath = "(//input[@class='form-control'])[2]")
-    public WebElement passwordTextBox;
+    private WebElement passwordTextBox;
     @FindBy (xpath = "//label[@class='form-check-label']")
-    public WebElement rememberMeBaslik;
+    private WebElement rememberMeBaslik;
     @FindBy (xpath = "//button[@type='submit']")
-    public WebElement adminLoginButton;
+    private WebElement adminLoginButton;
     @FindBy (xpath = "//a[@class='forget-text']")
     public WebElement forgotPasswordBaslik;
     @FindBy (xpath = "//h6[@class='page-title']")
     public WebElement adminDashoardyazisi;
 
-    ////
+    public void adminLoginGirisYap(String adminname,String adminPassword){
+        usernameTextBox.sendKeys(adminname);
+        passwordTextBox.sendKeys(adminPassword);
+        adminLoginButton.click();
+        ReusableMethods.bekle(2);
 
-    //bu bir cakısmadır
+    }
+
 }
